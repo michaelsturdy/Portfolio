05cc61e045e4bde57b9c6ef8635ce5a7efeddcc8
Mike Sturdy <w0290943@nscc.ca>
2017-01-06 13:19:29 -0400

    assg1 start

---
+++ MikeSturdyAssignment1/ChatConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatLib;
+
+namespace ChatLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0 && args[0] == "-server")
+            {
+                Server server = new Server();
+            }
+            else
+            {
+                Client client = new Client();
+            }
+
+        }
+    }
+}
