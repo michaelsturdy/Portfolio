85fbff98a32001799087fb382df84167515da627
Mike Sturdy <w0290943@nscc.ca>
2017-02-03 13:21:51 -0400

    finished commenting and turned on xml docs

--- ProgAssignment2/ChatLib/MessageReceivedEventArgs.cs
+++ ProgAssignment2/ChatLib/MessageReceivedEventArgs.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace ChatLib
 {
+    /// <summary>
+    /// handles messages from another thread
+    /// </summary>
     public class MessageReceivedEventArgs : EventArgs
     {
+        /// <summary>
+        /// Received message
+        /// </summary>
         public string Message { get; }
+
+        /// <summary>
+        /// Sets the Message Property
+        /// </summary>
+        /// <param name="message">Message to set to the property</param>
         public MessageReceivedEventArgs(string message)
         {
             Message = message;
