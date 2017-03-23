b804176f1ff25317d5ad5c4c9e015d326f69f31c
Mike Sturdy <w0290943@nscc.ca>
2017-03-23 13:20:19 -0300

    removed unnecessary usings

--- HungryHippo/HungryHippo/Hippo.cs
+++ HungryHippo/HungryHippo/Hippo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace HungryHippo
 {
@@ -35,7 +30,7 @@ namespace HungryHippo
        /// <summary>
        /// draws the hippo
        /// </summary>
-       /// <param name="graphics">graphics from paint event args</param>
+       /// <param name="graphics">graphics from paint event arguments</param>
         public void Draw(Graphics graphics)
         { 
             graphics.DrawImage(hippoPic, hippoDisplayArea);
